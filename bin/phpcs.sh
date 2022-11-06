f5936738b9aa2d0f41014ba66d6fd0df4b87f7ff
Dmitry Rechkin <rechkin@gmail.com>
2022-11-05 21:37:51 -0700

    added support for includes folder

--- bin/phpcs.sh
+++ bin/phpcs.sh
@@ -9,6 +9,10 @@ if [[ -d $PROJECT_DIR/src ]]; then
 	$BIN_DIR/phpcs --report=code --parallel=7 --cache --standard=$STANDARD_FILEPATH $PROJECT_DIR/src
 fi
 
+if [[ -d $PROJECT_DIR/includes ]]; then
+	$BIN_DIR/phpcs --report=code --parallel=7 --cache --standard=$STANDARD_FILEPATH $PROJECT_DIR/includes
+fi
+
 if [[ -d $PROJECT_DIR/tests ]]; then
 	$BIN_DIR/phpcs --report=code --parallel=7 --cache --standard=$STANDARD_FILEPATH $PROJECT_DIR/tests
 fi
